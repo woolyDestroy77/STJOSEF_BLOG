48535f89544beb63899b94662f566a00e840c341
woolyDestroy7 <120059280+woolyDestroy7@users.noreply.github.com>
2024-11-13 16:10:27 +0200

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express'
+import mongoos from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoos
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log('MongoDb is connected')
+    })
+    .catch(err => {
+        console.log(err)
+    })
 
 const app = express();
 
